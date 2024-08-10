0f3651a27eaba6a7ac841d473a74b0704d5f1111
Artem-Danilov <149016914+Artem-Danilov@users.noreply.github.com>
2024-08-10 23:18:35 +0300

    Update datebase.php

--- config/datebase.php
+++ config/datebase.php
@@ -1,10 +1,10 @@
 <?
 $server = 'localhost';
-$login = 'root';
-$pass = '';
-$nameDb = 'testbd';
+$login = 'dartem2005';
+$pass = 'Temoha2003';
+$nameDb = 'dartem2005';
 
 $connect = mysqli_connect($server, $login, $pass, $nameDb);
 if ($connect == false) {
 	echo("Error: Unable to connect to MySQL " . mysqli_connect_error());
-} 
+}
